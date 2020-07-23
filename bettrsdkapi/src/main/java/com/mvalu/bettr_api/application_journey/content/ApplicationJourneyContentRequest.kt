777b17f26e0cb36800ebc36cf831a7e75e195372
aman <yadavaman799@gmail.com>
2020-07-23 15:36:16 +0530

    application journey content api integration

--- bettrsdkapi/src/main/java/com/mvalu/bettr_api/application_journey/content/ApplicationJourneyContentRequest.kt
+++ bettrsdkapi/src/main/java/com/mvalu/bettr_api/application_journey/content/ApplicationJourneyContentRequest.kt
@@ -4,5 +4,9 @@ import com.squareup.moshi.Json
 
 class ApplicationJourneyContentRequest {
     @field:Json(name = "language")
-    var language: String? = null //"en||hi"
+    var language: String = "en" //"en||hi"
+
+    @field:Json(name = "placeholder")
+    var placeholder: String =
+        "bureau_cc_consent,location_cc_consent,job_detail_cc_consent,payment_cc_consent,account_info_cc_consent,document_submission_cc_consent,reject_message_cc"
 }
