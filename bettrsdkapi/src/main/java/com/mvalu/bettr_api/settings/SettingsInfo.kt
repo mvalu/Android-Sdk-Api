cbe6b849954e858fcaac9d11ae4eb0cd66a9ad59
aman <yadavaman799@gmail.com>
2020-08-19 18:34:22 +0530

    payment summary api integration

--- bettrsdkapi/src/main/java/com/mvalu/bettr_api/settings/SettingsInfo.kt
+++ bettrsdkapi/src/main/java/com/mvalu/bettr_api/settings/SettingsInfo.kt
@@ -3,12 +3,13 @@ package com.mvalu.bettr_api.settings
 import com.mvalu.bettr_api.home_module.AccountInfo
 import com.mvalu.bettr_api.home_module.CardInfo
 import com.mvalu.bettr_api.home_module.DueInfo
+import com.mvalu.bettr_api.payment.summary.PaymentDueInfo
 import com.squareup.moshi.Json
 
 class SettingsInfo {
 
     @field:Json(name = "dueInfo")
-    var dueInfo: DueInfo? = null
+    var dueInfo: PaymentDueInfo? = null
 
     @field:Json(name = "accountInfo")
     var accountInfo: AccountInfo? = null
