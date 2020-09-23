9c687eda75713e6b1ccdc5e0633a48b475191edb
aman <yadavaman799@gmail.com>
2020-09-23 23:32:20 +0530

    base url changes to prod

--- bettrsdkapi/src/main/java/com/mvalu/bettr_api/utils/BettrConstant.kt
+++ bettrsdkapi/src/main/java/com/mvalu/bettr_api/utils/BettrConstant.kt
@@ -4,7 +4,7 @@ package com.mvalu.bettr_api.utils
 //const val BASE_URL = "https://development-api.docker.mvalu.ml/api/"
 
 //pre-prod
-const val BASE_URL = "https://api-new.mvalu.ml/api/"
+//const val BASE_URL = "https://api-new.mvalu.ml/api/"
 
 //prod
-//const val BASE_URL = "https://development-api.docker.mvalu.ml/api/"
+const val BASE_URL = "https://api.mvalu.ml/api/"
