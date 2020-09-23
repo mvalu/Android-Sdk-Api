39ab2667c25fc13b496adaee738ace281168a0d5
aman <yadavaman799@gmail.com>
2020-09-23 23:45:40 +0530

    base url changes to prod

--- app/src/main/java/com/mvalu/androidsdkapi/MainActivity.kt
+++ app/src/main/java/com/mvalu/androidsdkapi/MainActivity.kt
@@ -3,28 +3,10 @@ package com.mvalu.androidsdkapi
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.mvalu.bettr_api.BettrApiSdk
 import com.mvalu.bettr_api.BettrApiSdkCallback
-import com.mvalu.bettr_api.home_module.HomeModule
-import com.mvalu.bettr_api.home_module.HomeModuleResults
-import com.mvalu.bettr_api.network.ApiResponseCallback
-import com.mvalu.bettr_api.transactions.CardTransactions
-import com.mvalu.bettr_api.transactions.CardTransactionsResults
 
 class MainActivity : AppCompatActivity(), BettrApiSdkCallback {
     override fun onSuccess() {
-        Toast.makeText(applicationContext, "Sdk initialized", Toast.LENGTH_SHORT).show()
-//        CardTransactions.getStatementTransactions(object : ApiResponseCallback<CardTransactionsResults> {
-//            override fun onSuccess(response: CardTransactionsResults) {
-//                TODO("Not yet implemented")
-//            }
-//
-//            override fun onError(errorMessage: String) {
-//                TODO("Not yet implemented")
-//            }
-//
-//
-//        }, )
     }
 
     override fun onError(error: String) {
@@ -34,28 +16,5 @@ class MainActivity : AppCompatActivity(), BettrApiSdkCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        BettrApiSdk.enableLoggingBehaviour(true)
-        BettrApiSdk.initializeSDK(
-            applicationContext,
-            "712ef65f-7b43-4240-8577-6e89f3bcbdcd",
-            "97ec49fc-f226-47dc-8fb9-1e0cfe227b2f",
-            "6f7016b5-ef39-4c1e-a6ce-dd069fb96c01",
-            "43",
-            null,
-            this
-        )
-
-//        CardUser.getUserDetails(CardUserRequest(), object : ApiResponseCallback<CardUserResponse>{
-//            override fun onSuccess(response: CardUserResponse) {
-//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//            }
-//
-//            override fun onError(errorMessage: String) {
-//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//            }
-//
-//        })
-
     }
 }
