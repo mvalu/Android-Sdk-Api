468816c5e06164961b898e382b5d131404c8cb61
Dhaval Rupapara <dhaval.rupapara14@gmail.com>
2021-04-28 16:45:08 +0530

    Added a minor change

--- bettrsdkapi/src/main/java/com/mvalu/bettr_api/emi/ConvertToEmiApiRequest.kt
+++ bettrsdkapi/src/main/java/com/mvalu/bettr_api/emi/ConvertToEmiApiRequest.kt
@@ -1,8 +1,9 @@
 package com.mvalu.bettr_api.emi
 
+import com.mvalu.bettr_api.network.ApiBaseRequest
 import com.squareup.moshi.Json
 
-class ConvertToEmiApiRequest(duration: Int) {
+class ConvertToEmiApiRequest(duration: Int) : ApiBaseRequest() {
 
     @field:Json(name = "duration")
     var duration: Int = duration
