70624ceff4842d3507b84137428295f3d9d2a600
INDIALENDS\deepakrawat <deepakrawat@indialends.com>
2021-11-17 12:58:06 +0530

    prod to dev

--- bettrsdkapi/src/main/java/com/mvalu/bettr_api/utils/BettrConstant.kt
+++ bettrsdkapi/src/main/java/com/mvalu/bettr_api/utils/BettrConstant.kt
@@ -1,13 +1,13 @@
 package com.mvalu.bettr_api.utils
 
 //dev
-//const val BASE_URL = "https://development-api.docker.mvalu.ml/api/"
+const val BASE_URL = "https://development-api.docker.mvalu.ml/api/"
 
 //pre-prod
 //const val BASE_URL = "https://api-new.mvalu.ml/api/"
 
 //prod
-const val BASE_URL = "https://api.mvalu.ml/api/"
+//const val BASE_URL = "https://api.mvalu.ml/api/"
 
 //testing
 //const val BASE_URL = "https://test-api.docker.mvalu.ml/api/"
